6d2c3a4d2bbeb1aa1600522b30af13b4c0d7506b
karthikeyan k <105141303+KarthikeyanKanniappan@users.noreply.github.com>
2022-11-27 21:41:37 +0530

    signIn_commit

--- controllers/user.js
+++ controllers/user.js
@@ -33,3 +33,29 @@ export const signup = async (req, res) => {
     console.log(err);
   }
 };
+
+export const signin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const oldUser = await UserModal.findOne({ email });
+    if (!oldUser)
+      return res.status(404).json({ message: "User doesn't exist" });
+
+    const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
+
+    if (!isPasswordCorrect)
+      return res.status(404).json({ message: "Invalid Credentials" });
+
+    const token = jwt.sign(
+      { email: oldUser.email, id: oldUser._id },
+      process.env.SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+    res.status(200).json({ oldUser, token });
+  } catch (err) {
+    res.status(500).json({ message: "something went wrong" });
+    console.log(err);
+  }
+};
