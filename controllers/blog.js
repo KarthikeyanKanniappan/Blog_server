3f1f459ffe1c29e90f22df23f356b6c3167dc641
karthikeyan k <105141303+KarthikeyanKanniappan@users.noreply.github.com>
2022-12-06 21:00:40 +0530

    blog commit

--- controllers/blog.js
+++ controllers/blog.js
@@ -2,10 +2,9 @@ import BlogModal from "../models/blog.js";
 
 export const createBlog = async (req, res) => {
   const blog = req.body;
-  console.log(blog);
   const newBlog = new BlogModal({
     ...blog,
-    // createdAt: new Date().toIsoString(),
+    createdAt: new Date().toISOString(),
   });
   try {
     await newBlog.save();
@@ -18,6 +17,16 @@ export const createBlog = async (req, res) => {
 export const getBlog = async (req, res) => {
   try {
     const blog = await BlogModal.find();
+    res.status(200).json(blog);
+  } catch (err) {
+    res.status(404).json({ message: "something went wrong" });
+  }
+};
+
+export const particularBlog = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const blog = await BlogModal.findById(id);
     res.status(200).json(blog);
   } catch (err) {
     res.status(404).json({ message: "something went wrong" });
--- routes/blog.js
+++ routes/blog.js
@@ -1,8 +1,10 @@
 import express from "express";
 const router = express.Router();
 
-import { createBlog, getBlog } from "../controllers/blog.js";
+import { createBlog, getBlog, particularBlog } from "../controllers/blog.js";
 
 router.post("/", createBlog);
 router.get("/", getBlog);
+router.get("/:id", particularBlog);
+
 export default router;
